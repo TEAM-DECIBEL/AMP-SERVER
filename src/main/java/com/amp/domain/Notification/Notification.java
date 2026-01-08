70536975800be267f3dd37aaad28fbb24f563163
Chae-Yu <chaeyu2004@naver.com>
2026-01-08 11:07:58 +0900

    feat: Notification entity 정의
    - 사용자에게 전송된 알람 관리

---
+++ src/main/java/com/amp/domain/Notification/Notification.java
@@ -0,0 +1,48 @@
+package com.amp.domain.Notification;
+
+import com.amp.domain.notice.entity.Notice;
+import com.amp.domain.user.entity.User;
+import com.amp.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice_id", nullable = false)
+    private Notice notice;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false;
+
+    @Builder
+    public Notification(User user, Notice notice, String title, String message) {
+        this.user = user;
+        this.notice = notice;
+        this.title = title;
+        this.message = message;
+        this.isRead = false;
+    }
+}
