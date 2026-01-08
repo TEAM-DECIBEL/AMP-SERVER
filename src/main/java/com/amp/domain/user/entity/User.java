51243f873b3d63f180c61f4ced1d67d5d5fa0ee9
Chae-Yu <chaeyu2004@naver.com>
2026-01-08 10:43:16 +0900

    feat: User entity 정의
    - 사용자 엔티티 정의
    - Oauth 관련 pr과 병합 예정

---
+++ src/main/java/com/amp/domain/user/entity/User.java
@@ -0,0 +1,20 @@
+package com.amp.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
