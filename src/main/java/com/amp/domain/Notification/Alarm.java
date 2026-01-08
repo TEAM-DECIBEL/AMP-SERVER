dc00259fbeb04bc38381653d436b82024fa7e3bc
Chae-Yu <chaeyu2004@naver.com>
2026-01-08 11:05:52 +0900

    feat: Alarm entity 정의
    - 알람 구독 설정 관리

---
+++ src/main/java/com/amp/domain/Notification/Alarm.java
@@ -0,0 +1,32 @@
+package com.amp.domain.Notification;
+
+import com.amp.domain.notice.entity.Category;
+import com.amp.domain.user.entity.User;
+import com.amp.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "alarm")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Alarm extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alarm_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+}
