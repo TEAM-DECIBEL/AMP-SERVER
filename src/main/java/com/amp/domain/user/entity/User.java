8e086ef775d983ca8e83f5a2027eb72090defecb
Chae-Yu <chaeyu2004@naver.com>
2026-01-09 16:55:53 +0900

    refactor: 칼럼 nullable 여부 추가

--- src/main/java/com/amp/domain/user/entity/User.java
+++ src/main/java/com/amp/domain/user/entity/User.java
@@ -18,16 +18,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false, name = "profile_image_url")
     private String profileImageUrl;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private AuthProvider provider;
 
+    @Column(nullable = false, name = "provider_id")
     private String providerId;
 
     @Enumerated(EnumType.STRING)
@@ -43,4 +47,3 @@ public class User {
     }
 
 }
-
