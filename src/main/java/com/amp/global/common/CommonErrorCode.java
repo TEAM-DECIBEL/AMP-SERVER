da8ec27b7b3a027893e0e5c242287409de8688ce
Choi Min Young <codingmy@naver.com>
2026-01-10 15:27:34 +0900

    [setting] 커밋 누락 코드 커밋

--- src/main/java/com/amp/global/common/CommonErrorCode.java
+++ src/main/java/com/amp/global/common/CommonErrorCode.java
@@ -14,7 +14,7 @@ public enum CommonErrorCode implements ErrorCode {
     INVALID_JSON(HttpStatus.BAD_REQUEST, "C004", "JSON 파싱 중 오류가 발생했습니다."),
     INVALID_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, "C405","이미지 파일만 업로드 가능합니다."),
     FILE_NAME_NOT_FOUND(HttpStatus.NOT_FOUND, "C406", "파일명이 없습니다."),
-    INVALID_DIRECTORY_ROUTE(HttpStatus.NOT_FOUND, "407", "잘못된 디렉토리 경로입니다."),
+    INVALID_DIRECTORY_ROUTE(HttpStatus.NOT_FOUND, "C407", "잘못된 디렉토리 경로입니다."),
 
     // 401 Unauthorized
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C005", "로그인이 필요한 서비스입니다."),
