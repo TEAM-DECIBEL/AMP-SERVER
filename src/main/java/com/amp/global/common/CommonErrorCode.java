498879e92ce2ac428029b6be71e3d8036a88c0d2
Choi Min Young <codingmy@naver.com>
2026-01-10 11:58:49 +0900

    [feat] errorcode 추가

--- src/main/java/com/amp/global/common/CommonErrorCode.java
+++ src/main/java/com/amp/global/common/CommonErrorCode.java
@@ -12,6 +12,9 @@ public enum CommonErrorCode implements ErrorCode {
     TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "C002", "입력값의 타입이 일치하지 않습니다."),
     MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "C003", "필수 파라미터가 누락되었습니다."),
     INVALID_JSON(HttpStatus.BAD_REQUEST, "C004", "JSON 파싱 중 오류가 발생했습니다."),
+    INVALID_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, "C405","이미지 파일만 업로드 가능합니다."),
+    FILE_NAME_NOT_FOUND(HttpStatus.NOT_FOUND, "C406", "파일명이 없습니다."),
+    INVALID_DIRECTORY_ROUTE(HttpStatus.NOT_FOUND, "407", "잘못된 디렉토리 경로입니다."),
 
     // 401 Unauthorized
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "C005", "로그인이 필요한 서비스입니다."),
