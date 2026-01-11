664727c4f47d99714a2e31d6fd6a010c31ae947b
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 08:57:14 +0900

    feat: festival repository 추가

---
+++ src/main/java/com/amp/domain/festival/repository/FestivalRepository.java
@@ -0,0 +1,9 @@
+package com.amp.domain.festival.repository;
+
+import com.amp.domain.festival.entity.Festival;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FestivalRepository extends JpaRepository<Festival, Long> {
+}
