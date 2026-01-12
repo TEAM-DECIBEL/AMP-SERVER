f3944241ed191402dbf91c0573e480290ff5abc7
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 16:05:12 +0900

    feat: category 관련 에러 코드 추가

---
+++ src/main/java/com/amp/domain/category/exception/CategoryErrorCode.java
@@ -0,0 +1,24 @@
+package com.amp.domain.category.exception;
+
+import com.amp.global.common.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CategoryErrorCode implements ErrorCode {
+
+    // 400 Bad Request
+    CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "CAT", "001", "존재하지 않는 카테고리입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String domain;
+    private final String numbering;
+    private final String msg;
+
+    @Override
+    public String getCode() {
+        return domain + "_" + httpStatus.value() + "_" + numbering; //
+    }
+}
