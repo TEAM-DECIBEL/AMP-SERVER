e37aa5867a7d978dd88a6054da4284b7b9c473f3
Chae-Yu <chaeyu2004@naver.com>
2026-01-13 01:41:42 +0900

    [refactor] 응답 코드 수정

--- src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
+++ src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
@@ -14,8 +14,8 @@ public enum FestivalErrorCode implements ErrorCode {
     FESTIVAL_CREATE_FAILED(HttpStatus.BAD_REQUEST, "FES", "002", "페스티벌 생성에 실패하였습니다."),
     SCHEDULES_REQUIRED(HttpStatus.BAD_REQUEST, "FES", "003", "하나 이상의 공연 일자 입력은 필수입니다."),
     INVALID_SCHEDULE_FORMAT(HttpStatus.BAD_REQUEST, "FES", "004", "공연 일자 형식이 잘못되었습니다."),
-    INVALID_STAGE_FORMAT(HttpStatus.BAD_REQUEST, "FES", "005", "무대/부스 형식이 잘못되었습니다"),
-    INVALID_CATEGORY_FORMAT(HttpStatus.BAD_REQUEST, "FES", "006", "무대/부스 형식이 잘못되었습니다"),
+    INVALID_STAGE_FORMAT(HttpStatus.BAD_REQUEST, "FES", "005", "무대/부스 형식이 잘못되었습니다."),
+    INVALID_CATEGORY_FORMAT(HttpStatus.BAD_REQUEST, "FES", "006", "카테고리 형식이 잘못되었습니다."),
 
     // 404 Not Found
     FESTIVAL_NOT_FOUND(HttpStatus.NOT_FOUND, "FES", "001", "존재하지 않는 공연 정보입니다.");
