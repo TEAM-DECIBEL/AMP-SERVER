95cf627ecae8b0c1c21b44e2c578e95064bd551b
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 16:05:18 +0900

    feat: festival 관련 에러 코드 추가

--- src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
+++ src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum FestivalErrorCode implements ErrorCode {
 
     // 400 Bad Request
-    INVALID_FESTIVAL_PERIOD(HttpStatus.BAD_REQUEST, "F", "001", "공연 종료일은 시작일보다 빠를 수 없습니다."),
-
+    INVALID_FESTIVAL_PERIOD(HttpStatus.BAD_REQUEST, "FES", "001", "공연 종료일은 시작일보다 빠를 수 없습니다."),
+    FESTIVAL_CREATE_FAILED(HttpStatus.BAD_REQUEST, "FES", "002", "페스티벌 생성에 실패하였습니다."),
     // 404 Not Found
-    FESTIVAL_NOT_FOUND(HttpStatus.NOT_FOUND, "F", "001", "존재하지 않는 공연 정보입니다.");
+    FESTIVAL_NOT_FOUND(HttpStatus.NOT_FOUND, "FES", "001", "존재하지 않는 공연 정보입니다.");
 
     private final HttpStatus httpStatus;
     private final String domain;
