2d9e59d2d4e39d3a3593f5716dafe70bc2328630
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 12:02:44 +0900

    feat: Getter 메소드 추가

--- src/main/java/com/amp/domain/festival/dto/request/ScheduleRequest.java
+++ src/main/java/com/amp/domain/festival/dto/request/ScheduleRequest.java
@@ -1,11 +1,13 @@
 package com.amp.domain.festival.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Getter
 public class ScheduleRequest {
     @NotNull(message = "공연 날짜는 필수입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
