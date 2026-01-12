3375154a9ce6e1034ac7d4461633b0ba003575ff
Choi Min Young <codingmy@naver.com>
2026-01-13 01:10:41 +0900

    [feat] 에러코드 추가

---
+++ src/main/java/com/amp/domain/announcement/exception/AnnouncementErrorCode.java
@@ -0,0 +1,30 @@
+package com.amp.domain.announcement.exception;
+
+import com.amp.global.common.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AnnouncementErrorCode implements ErrorCode {
+    // 400 Bad Request
+    INVALID_ANNOUNCEMENT(HttpStatus.BAD_REQUEST, "ANN", "001", "잘못된 공지 값입니다.");
+
+    // 401 Unauthorized
+
+    // 403 Forbidden
+
+    // 405 Method Not Allowed
+    // 500 Internal Server Error
+
+    private final HttpStatus httpStatus;
+    private final String domain;
+    private final String numbering;
+    private final String msg;
+
+    @Override
+    public String getCode() {
+        return domain + "_" + httpStatus.value() + "_" + numbering;
+    }
+}
