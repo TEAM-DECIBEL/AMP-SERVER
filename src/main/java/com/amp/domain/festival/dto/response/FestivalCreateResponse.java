f67ad558e9069e98b46c9e64ccf26c7966dbc8f9
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 16:03:54 +0900

    feat: FestivalCreateRequest from 메서드 추가

--- src/main/java/com/amp/domain/festival/dto/response/FestivalCreateResponse.java
+++ src/main/java/com/amp/domain/festival/dto/response/FestivalCreateResponse.java
@@ -1,13 +1,25 @@
 package com.amp.domain.festival.dto.response;
 
+import com.amp.domain.festival.entity.Festival;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class FestivalCreateResponse {
     private Long festivalId;
     private String mainImageUrl;
     private String title;
     private LocalDateTime createAt;
+
+    public static FestivalCreateResponse from(Festival festival) {
+        return FestivalCreateResponse.builder()
+                .festivalId(festival.getId())
+                .title(festival.getTitle())
+                .mainImageUrl(festival.getMainImageUrl())
+                .createAt(festival.getCreatedAt())
+                .build();
+    }
 }
