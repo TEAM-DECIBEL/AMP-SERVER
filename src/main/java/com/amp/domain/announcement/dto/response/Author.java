5e602a34f1ef1d8f1185d535d6d6b191cfc5796a
Choi Min Young <codingmy@naver.com>
2026-01-13 01:09:54 +0900

    [feat] 공지 조회 dto 추가

---
+++ src/main/java/com/amp/domain/announcement/dto/response/Author.java
@@ -0,0 +1,6 @@
+package com.amp.domain.announcement.dto.response;
+
+public record Author(
+        Long userId,
+        String nickname
+) {}
---
+++ src/main/java/com/amp/domain/announcement/dto/response/CategoryData.java
@@ -0,0 +1,7 @@
+package com.amp.domain.announcement.dto.response;
+
+public record CategoryData(
+        Long categoryId,
+        String categoryName,
+        String categoryCode
+) {}
