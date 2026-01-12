4754a6df2a77e1810e727892bcab2169a5e8063c
Chae-Yu <chaeyu2004@naver.com>
2026-01-13 01:16:58 +0900

    [refactor] record로 변경

--- src/main/java/com/amp/domain/festival/dto/response/FestivalCreateResponse.java
+++ src/main/java/com/amp/domain/festival/dto/response/FestivalCreateResponse.java
@@ -1,25 +1,21 @@
 package com.amp.domain.festival.dto.response;
 
 import com.amp.domain.festival.entity.Festival;
-import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Builder
-public class FestivalCreateResponse {
-    private Long festivalId;
-    private String mainImageUrl;
-    private String title;
-    private LocalDateTime createdAt;
-
+public record FestivalCreateResponse(
+        Long festivalId,
+        String mainImageUrl,
+        String title,
+        LocalDateTime createdAt
+) {
     public static FestivalCreateResponse from(Festival festival) {
-        return FestivalCreateResponse.builder()
-                .festivalId(festival.getId())
-                .title(festival.getTitle())
-                .mainImageUrl(festival.getMainImageUrl())
-                .createdAt(festival.getCreatedAt())
-                .build();
+        return new FestivalCreateResponse(
+                festival.getId(),
+                festival.getMainImageUrl(),
+                festival.getTitle(),
+                festival.getCreatedAt()
+        );
     }
 }
