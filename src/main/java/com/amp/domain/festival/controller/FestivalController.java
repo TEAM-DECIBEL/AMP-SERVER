fd5c43cc4afeda7b8e7f2beef8f4098523a41260
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 16:06:44 +0900

    feat: ModelAttribute 어노테이션 추가

--- src/main/java/com/amp/domain/festival/controller/FestivalController.java
+++ src/main/java/com/amp/domain/festival/controller/FestivalController.java
@@ -3,27 +3,25 @@ package com.amp.domain.festival.controller;
 import com.amp.domain.festival.dto.request.FestivalCreateRequest;
 import com.amp.domain.festival.dto.response.FestivalCreateResponse;
 import com.amp.domain.festival.service.FestivalService;
+import com.amp.global.common.SuccessStatus;
 import com.amp.global.response.success.BaseResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/vi/festivals")
+@RequestMapping("/api/v1/festivals")
 @RequiredArgsConstructor
 public class FestivalController {
 
     private final FestivalService festivalService;
 
-    public BaseResponse<FestivalCreateResponse> createFestival(
-            @RequestPart("request") @Valid FestivalCreateRequest request,
-            @RequestPart("image") MultipartFile image) {
-
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public BaseResponse<FestivalCreateResponse> createFestival
+            (@ModelAttribute @Valid FestivalCreateRequest request) {
         FestivalCreateResponse response = festivalService.createFestival(request);
-        return BaseResponse.create("dd", response);
+        return BaseResponse.create(SuccessStatus.FESTIVAL_CREATE_SUCCESS.getMsg(), response);
     }
 
 }
