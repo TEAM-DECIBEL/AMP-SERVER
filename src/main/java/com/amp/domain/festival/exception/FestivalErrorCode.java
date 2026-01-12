878963c9a8848844562d038483d12d1c79dd4f0b
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 23:35:46 +0900

    [feat] festival 에러코드 추가

--- src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
+++ src/main/java/com/amp/domain/festival/exception/FestivalErrorCode.java
@@ -12,6 +12,11 @@ public enum FestivalErrorCode implements ErrorCode {
     // 400 Bad Request
     INVALID_FESTIVAL_PERIOD(HttpStatus.BAD_REQUEST, "FES", "001", "공연 종료일은 시작일보다 빠를 수 없습니다."),
     FESTIVAL_CREATE_FAILED(HttpStatus.BAD_REQUEST, "FES", "002", "페스티벌 생성에 실패하였습니다."),
+    SCHEDULES_REQUIRED(HttpStatus.BAD_REQUEST, "FES", "003", "하나 이상의 공연 일자 입력은 필수입니다."),
+    INVALID_SCHEDULE_FORMAT(HttpStatus.BAD_REQUEST, "FES", "004", "공연 일자 형식이 잘못되었습니다."),
+    INVALID_STAGE_FORMAT(HttpStatus.BAD_REQUEST, "FES", "005", "무대/부스 형식이 잘못되었습니다"),
+    INVALID_CATEGORY_FORMAT(HttpStatus.BAD_REQUEST, "FES", "006", "무대/부스 형식이 잘못되었습니다"),
+
     // 404 Not Found
     FESTIVAL_NOT_FOUND(HttpStatus.NOT_FOUND, "FES", "001", "존재하지 않는 공연 정보입니다.");
 
