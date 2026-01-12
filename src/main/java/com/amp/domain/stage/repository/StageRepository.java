70cd2776d88d5299495bc0b55cc8880fe0ce2b6e
Chae-Yu <chaeyu2004@naver.com>
2026-01-13 01:18:32 +0900

    [style] 불필요한 어노테이션 삭제

--- src/main/java/com/amp/domain/stage/repository/StageRepository.java
+++ src/main/java/com/amp/domain/stage/repository/StageRepository.java
@@ -4,6 +4,5 @@ import com.amp.domain.stage.entity.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StageRepository extends JpaRepository<Stage, Long> {
 }
