29a6413937a16497d0bc3a4950dd4aa04af9b2ce
Chae-Yu <chaeyu2004@naver.com>
2026-01-12 16:06:54 +0900

    feat: JSON 포맷으로 입력

--- src/main/java/com/amp/domain/festival/dto/request/ScheduleRequest.java
+++ src/main/java/com/amp/domain/festival/dto/request/ScheduleRequest.java
@@ -1,19 +1,21 @@
 package com.amp.domain.festival.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
+@NoArgsConstructor
 public class ScheduleRequest {
     @NotNull(message = "공연 날짜는 필수입니다.")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate festivalDate;
 
     @NotNull(message = "공연 시간은 필수입니다.")
-    @DateTimeFormat(pattern = "HH:mm")
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime festivalTime;
 }
