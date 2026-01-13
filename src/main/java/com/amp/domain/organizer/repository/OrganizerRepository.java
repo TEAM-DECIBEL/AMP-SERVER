5cfa87e38607466b9462631f7d6a014092a553f9
Chae-Yu <chaeyu2004@naver.com>
2026-01-13 21:51:25 +0900

    [feat] OrganizerRepository 작성

--- src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
+++ src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
@@ -1,7 +1,10 @@
 package com.amp.domain.organizer.repository;
 
+import com.amp.domain.festival.entity.Festival;
 import com.amp.domain.organizer.entity.Organizer;
+import com.amp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+    boolean existsByFestivalAndUser(Festival festival, User user);
 }
