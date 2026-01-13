80af2eef4035053d59e53203ad9e52796bf88e93
Choi Min Young <codingmy@naver.com>
2026-01-14 04:58:26 +0900

    [feat] 에러코드 추가

--- src/main/java/com/amp/domain/notice/exception/UserSavedNoticeErrorCode.java
+++ src/main/java/com/amp/domain/notice/exception/UserSavedNoticeErrorCode.java
@@ -9,15 +9,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserSavedNoticeErrorCode implements ErrorCode {
     // 400 Bad Request
-
-    // 401 Unauthorized
-
-    // 403 Forbidden
+    NOTICE_ALREADY_BOOKMARKED(HttpStatus.BAD_REQUEST, "SAV", "001", "이미 저장한 공지입니다."),
+    NOTICE_NOT_BOOKMARKED(HttpStatus.BAD_REQUEST, "SAV", "001", "이미 저장한 공지입니다."),
 
     // 404 Not Found
     SAVED_NOTICE_NOT_EXIST(HttpStatus.NOT_FOUND, "SAV", "001", "해당 유저가 저장한 공지가 아닙니다");
-    // 405 Method Not Allowed
-    // 500 Internal Server Error
 
     private final HttpStatus httpStatus;
     private final String domain;
