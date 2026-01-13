3377f646bdf30d549308a1296717b80c0043eb62
Chae-Yu <chaeyu2004@naver.com>
2026-01-13 23:23:39 +0900

    [feat] FestUpdate DTO 작성

---
+++ src/main/java/com/amp/domain/festival/dto/request/FestivalUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.amp.domain.festival.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+public record FestivalUpdateRequest(
+        @NotBlank(message = "공연명은 필수입니다.") String title,
+        @NotBlank(message = "공연 장소는 필수입니다.") String location,
+        @NotBlank(message = "공연 일시는 필수입니다.") String schedules,
+        String stages,
+        @NotEmpty(message = "최소 1개의 카테고리를 선택해야 합니다.") List<Long> activeCategoryIds
+) {
+}
