5b8ed521d8e9096044b1f1ce8d746538240c0ebf
Choi Min Young <codingmy@naver.com>
2026-01-14 04:56:37 +0900

    [feat] dto 어노테이션 추가

--- src/main/java/com/amp/domain/notice/dto/request/NoticeSaveRequest.java
+++ src/main/java/com/amp/domain/notice/dto/request/NoticeSaveRequest.java
@@ -1,6 +1,9 @@
 package com.amp.domain.notice.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record NoticeSaveRequest(
+        @NotNull
         boolean isBookmarked
 ) {
 }
