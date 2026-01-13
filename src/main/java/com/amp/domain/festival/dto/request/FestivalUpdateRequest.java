b26a0cfa8762d0e2d50cd32b49327f3a9da8a9ce
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 01:09:28 +0900

    [refactor] String으로 받아서 파싱 -> List 형식으로 변환

--- src/main/java/com/amp/domain/festival/dto/request/FestivalUpdateRequest.java
+++ src/main/java/com/amp/domain/festival/dto/request/FestivalUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.amp.domain.festival.dto.request;
 
+import com.amp.domain.stage.dto.request.StageRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
@@ -8,8 +9,8 @@ import java.util.List;
 public record FestivalUpdateRequest(
         @NotBlank(message = "공연명은 필수입니다.") String title,
         @NotBlank(message = "공연 장소는 필수입니다.") String location,
-        @NotBlank(message = "공연 일시는 필수입니다.") String schedules,
-        String stages,
+        @NotBlank(message = "공연 일시는 필수입니다.") List<ScheduleRequest> schedules,
+        List<StageRequest> stages,
         @NotEmpty(message = "최소 1개의 카테고리를 선택해야 합니다.") List<Long> activeCategoryIds
 ) {
 }
