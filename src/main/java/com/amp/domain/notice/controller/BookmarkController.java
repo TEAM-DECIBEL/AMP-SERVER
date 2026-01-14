b7f1857772d17da30a5e17e0bf146338a4645685
Choi Min Young <codingmy@naver.com>
2026-01-14 15:40:23 +0900

    [rename] 리네이밍 적용

---
+++ src/main/java/com/amp/domain/notice/controller/BookmarkController.java
@@ -0,0 +1,37 @@
+package com.amp.domain.notice.controller;
+
+import com.amp.domain.notice.dto.request.BookmarkRequest;
+import com.amp.domain.notice.dto.response.BookmarkResponse;
+import com.amp.domain.notice.service.BookmarkService;
+import com.amp.global.common.SuccessStatus;
+import com.amp.global.response.success.BaseResponse;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/notices")
+@RequiredArgsConstructor
+@Slf4j
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    // 공지 북마크 업데이트
+    @PostMapping("/{noticeId}/bookmark")
+    public ResponseEntity<BaseResponse<BookmarkResponse>> saveNotice(
+            @PathVariable("noticeId") @Positive Long noticeId,
+            @RequestBody BookmarkRequest bookmarkRequest
+    ) {
+        BookmarkResponse response =
+                bookmarkService.updateBookmark(noticeId, bookmarkRequest);
+
+        return ResponseEntity
+                .status(SuccessStatus.BOOKMARK_UPDATE_SUCCESS.getHttpStatus())
+                .body(BaseResponse.create(SuccessStatus.BOOKMARK_UPDATE_SUCCESS.getMsg(), response));
+    }
+
+}
+
