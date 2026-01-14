3acb225bbf75a97f293a19658cc30a84c7cc5994
Choi Min Young <codingmy@naver.com>
2026-01-15 04:31:09 +0900

    [refactor] url 차이로 인한 클래스 분리

--- src/main/java/com/amp/domain/notice/controller/NoticeController.java
+++ src/main/java/com/amp/domain/notice/controller/NoticeController.java
@@ -1,7 +1,5 @@
 package com.amp.domain.notice.controller;
 
-import com.amp.domain.notice.dto.request.NoticeCreateRequest;
-import com.amp.domain.notice.dto.response.NoticeCreateResponse;
 import com.amp.domain.notice.dto.response.NoticeDetailResponse;
 import com.amp.domain.notice.service.NoticeService;
 import com.amp.global.annotation.ApiErrorCodes;
@@ -9,10 +7,8 @@ import com.amp.global.common.SuccessStatus;
 import com.amp.global.response.success.BaseResponse;
 import com.amp.global.swagger.SwaggerResponseDescription;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,19 +20,6 @@ import org.springframework.web.bind.annotation.*;
 public class NoticeController {
 
     private final NoticeService noticeService;
-
-    @Operation(summary = "공지 생성")
-    @ApiErrorCodes(SwaggerResponseDescription.FAIL_TO_CREATE_NOTICE)
-    @PostMapping(path = "/{festivalId}",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<BaseResponse<NoticeCreateResponse>> createNotice(
-            @PathVariable("festivalId") @Positive Long festivalId,
-            @ModelAttribute @Valid NoticeCreateRequest request) {
-        NoticeCreateResponse response = noticeService.createNotice(festivalId, request);
-        return ResponseEntity
-                .status(SuccessStatus.NOTICE_CREATE_SUCCESS.getHttpStatus())
-                .body(BaseResponse.create(SuccessStatus.NOTICE_CREATE_SUCCESS.getMsg(), response));
-    }
 
     @Operation(summary = "공지 상세 조회")
     @ApiErrorCodes(SwaggerResponseDescription.FAIL_TO_GET_NOTICE_DETAIL)
