085b9a5de6669eb07fcbade3f478b980f6f6da15
Choi Min Young <codingmy@naver.com>
2026-01-14 16:27:05 +0900

    [fix] 어노테이션 삭제

--- src/main/java/com/amp/domain/notice/service/BookmarkService.java
+++ src/main/java/com/amp/domain/notice/service/BookmarkService.java
@@ -19,12 +19,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @AllArgsConstructor
-@Transactional(readOnly = true)
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
