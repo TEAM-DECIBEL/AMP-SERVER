08f855c3cb5780bde0e945454ec7fc1490304be4
Choi Min Young <codingmy@naver.com>
2026-01-15 04:31:09 +0900

    [refactor] url 차이로 인한 클래스 분리

--- src/main/java/com/amp/domain/notice/dto/request/NoticeCreateRequest.java
+++ src/main/java/com/amp/domain/notice/dto/request/NoticeCreateRequest.java
@@ -1,6 +1,7 @@
 package com.amp.domain.notice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,7 +11,7 @@ public record NoticeCreateRequest(
         @NotBlank(message = "공지 카테고리 값은 필수값입니다.") String categoryId,
         MultipartFile image,
         @NotBlank(message = "공지 제목은 필수값입니다.") String content,
-        @NotBlank(message = "고정 여부 값은 필수 값입니다.") boolean isPinned
+        @NotNull(message = "고정 여부 값은 필수 값입니다.") boolean isPinned
 ) {
 }
 
