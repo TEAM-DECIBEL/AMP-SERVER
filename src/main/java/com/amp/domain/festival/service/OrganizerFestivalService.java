b7f2e8205f14f42a542ada4b9b49e819dc99a61f
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 18:40:43 +0900

    [feat] 진행 중/예정 service 로직 작성

--- src/main/java/com/amp/domain/festival/service/OrganizerFestivalService.java
+++ src/main/java/com/amp/domain/festival/service/OrganizerFestivalService.java
@@ -1,8 +1,10 @@
 package com.amp.domain.festival.service;
 
+import com.amp.domain.festival.dto.response.ActiveFestivalPageResponse;
 import com.amp.domain.festival.dto.response.FestivalPageResponse;
 import com.amp.domain.festival.dto.response.FestivalSummaryResponse;
 import com.amp.domain.festival.entity.Festival;
+import com.amp.domain.festival.entity.FestivalStatus;
 import com.amp.domain.festival.repository.FestivalRepository;
 import com.amp.domain.user.entity.User;
 import com.amp.global.security.service.AuthService;
@@ -27,5 +29,16 @@ public class OrganizerFestivalService {
         Page<Festival> festivalPage = festivalRepository.findAllByMyUser(user, pageable);
         Page<FestivalSummaryResponse> summaryPage = festivalPage.map(FestivalSummaryResponse::from);
         return FestivalPageResponse.of(summaryPage);
+    }
+
+    @Transactional(readOnly = true)
+    public ActiveFestivalPageResponse getActiveFestivals(Pageable pageable) {
+        User user = authService.getCurrentUser();
+
+        long ongoingCount = festivalRepository.countByOrganizerAndStatus(user, FestivalStatus.ONGOING);
+        long upcomingCount = festivalRepository.countByOrganizerAndStatus(user, FestivalStatus.UPCOMING);
+
+        Page<Festival> activePage = festivalRepository.findActiveFestivalsByUser(user, pageable);
+        return ActiveFestivalPageResponse.of(ongoingCount, upcomingCount, activePage);
     }
 }
