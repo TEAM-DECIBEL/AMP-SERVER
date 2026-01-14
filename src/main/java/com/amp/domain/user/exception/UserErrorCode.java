825365ec94f14d9e3019acdc3ee66580b17b83d9
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 22:20:24 +0900

    [fix] 에러코드 http status 변경

--- src/main/java/com/amp/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/amp/domain/user/exception/UserErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-    // 400 Bad Request
-    USER_NOT_AUTHENTICATED(HttpStatus.BAD_REQUEST, "USE", "001", "유효하지 않은 권한입니다."),
+    // 403 FORBIDDEN
+    USER_NOT_AUTHENTICATED(HttpStatus.FORBIDDEN, "USE", "001", "유효하지 않은 권한입니다."),
 
     // 404 Not Found
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USE", "001", "존재하지 않는 유저 정보입니다.");
