f1c078248255d14e548595ec0285c96489188468
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 16:25:16 +0900

    [feat] 주최사가 등록한 모든 공연 조회 controller 작성

--- src/main/java/com/amp/global/common/SuccessStatus.java
+++ src/main/java/com/amp/global/common/SuccessStatus.java
@@ -17,6 +17,9 @@ public enum SuccessStatus implements SuccessCode {
     FESTIVAL_UPDATE_SUCCESS(HttpStatus.OK, "FES", "003", "공연 정보 수정이 완료되었습니다."),
     FESTIVAL_DELETE_SUCCESS(HttpStatus.OK, "FES", "004", "공연 삭제가 완료되었습니다."),
 
+    // ORGANIZER
+    GET_MY_ALL_FESTIVALS(HttpStatus.OK, "ORG", "001", "나의 진행한 모든 공연 조회가 완료되었습니다."),
+
     // NOTICE
     NOTICE_DETAIL_GET_SUCCESS(HttpStatus.OK, "NOT", "001", "공지 상세 조회가 완료되었습니다.");
 
