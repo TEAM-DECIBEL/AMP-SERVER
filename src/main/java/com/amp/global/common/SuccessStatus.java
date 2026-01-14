e90b4d54724cfb0e7699d214e4b377f28167000f
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 18:40:23 +0900

    [feat] 진행 중/예정 controller 작성

--- src/main/java/com/amp/global/common/SuccessStatus.java
+++ src/main/java/com/amp/global/common/SuccessStatus.java
@@ -19,6 +19,7 @@ public enum SuccessStatus implements SuccessCode {
 
     // ORGANIZER
     GET_MY_ALL_FESTIVALS(HttpStatus.OK, "ORG", "001", "나의 진행한 모든 공연 조회가 완료되었습니다."),
+    GET_MY_ALL_ACTIVE_FESTIVALS(HttpStatus.OK, "ORG", "002", "진행 중 및 진행 예정 공연 조회가 완료되었습니다."),
 
     // NOTICE
     NOTICE_DETAIL_GET_SUCCESS(HttpStatus.OK, "NOT", "001", "공지 상세 조회가 완료되었습니다.");
