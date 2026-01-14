9259319e75797bc5fc6333fd67115bc6a9d67eea
Chae-Yu <chaeyu2004@naver.com>
2026-01-14 09:38:28 +0900

    [feat] bulk insert로 연관 관계 소프트 딜리트

--- src/main/java/com/amp/domain/stage/repository/StageRepository.java
+++ src/main/java/com/amp/domain/stage/repository/StageRepository.java
@@ -2,7 +2,13 @@ package com.amp.domain.stage.repository;
 
 import com.amp.domain.stage.entity.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface StageRepository extends JpaRepository<Stage, Long> {
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Stage s SET s.deletedAt = CURRENT_TIMESTAMP WHERE s.festival.id = :festivalId AND s.deletedAt IS NULL")
+    void softDeleteByFestivalId(@Param("festivalId") Long festivalId);
 }
