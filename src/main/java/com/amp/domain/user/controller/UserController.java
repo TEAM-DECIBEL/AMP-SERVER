6e4f2215a4c950bf9db4bb2bf7c591491f51fe94
dongchan0105 <come753zz@gmail.com>
2026-01-14 23:11:59 +0900

    [feat] MyPage 조회하는 MVC로직 작성 이때 접근 중인 user정보는 CustomUserPrincipal에서 어노테이션을 통해 조회

--- src/main/java/com/amp/domain/user/controller/UserController.java
+++ src/main/java/com/amp/domain/user/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.amp.domain.user.controller;
 
+
+import com.amp.domain.user.dto.MyPageResponse;
+import com.amp.domain.user.service.UserService;
+import com.amp.global.common.SuccessStatus;
+import com.amp.global.response.success.BaseResponse;
+import com.amp.global.security.CustomUserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<BaseResponse<MyPageResponse>> getMyPage(
+            @AuthenticationPrincipal CustomUserPrincipal principal) {
+        Long userId = principal.getUserId();
+        MyPageResponse response = userService.getMyPage(userId);
+
+        return ResponseEntity
+                .status(SuccessStatus.OK.getHttpStatus())
+                .body(BaseResponse.of(SuccessStatus.OK, response));
+    }
 }
--- src/main/java/com/amp/domain/user/dto/MyPageResponse.java
+++ src/main/java/com/amp/domain/user/dto/MyPageResponse.java
@@ -1,4 +1,22 @@
 package com.amp.domain.user.dto;
 
-public class MyPageResponse {
+import com.amp.domain.user.entity.User;
+import com.amp.domain.user.entity.UserType;
+import lombok.Builder;
+
+@Builder
+public record MyPageResponse(
+        Long userId,
+        String nickname,
+        String profileImageUrl,
+        UserType userType
+) {
+    public static MyPageResponse from(User user) {
+        return MyPageResponse.builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfileImageUrl())
+                .userType(user.getUserType())
+                .build();
+    }
 }
