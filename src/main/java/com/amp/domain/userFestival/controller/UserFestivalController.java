706b944d9fd6d848f92350b4e87149e79007232d
Chae-Yu <chaeyu2004@naver.com>
2026-01-16 02:23:04 +0900

    [refactor] 와일드 카드 import 문 삭제

--- src/main/java/com/amp/domain/userFestival/controller/UserFestivalController.java
+++ src/main/java/com/amp/domain/userFestival/controller/UserFestivalController.java
@@ -14,9 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
