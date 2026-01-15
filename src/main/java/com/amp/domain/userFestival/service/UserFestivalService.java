7e4e95fee6f0b867578d9af08b2e22f52238d993
Chae-Yu <chaeyu2004@naver.com>
2026-01-15 17:03:23 +0900

    [feat] 관람 공연 등록 및 해제 API service 로직 작성

--- src/main/java/com/amp/domain/userFestival/service/UserFestivalService.java
+++ src/main/java/com/amp/domain/userFestival/service/UserFestivalService.java
@@ -1,11 +1,16 @@
 package com.amp.domain.userFestival.service;
 
+import com.amp.domain.festival.entity.UserFestival;
+import com.amp.domain.festival.exception.FestivalErrorCode;
+import com.amp.domain.userFestival.dto.request.WishListRequest;
 import com.amp.domain.userFestival.dto.response.UserFestivalListResponse;
 import com.amp.domain.userFestival.dto.response.UserFestivalPageResponse;
 import com.amp.domain.festival.entity.Festival;
 import com.amp.domain.festival.repository.FestivalRepository;
 import com.amp.domain.user.entity.User;
+import com.amp.domain.userFestival.dto.response.WishListResponse;
 import com.amp.domain.userFestival.repository.UserFestivalRepository;
+import com.amp.global.exception.CustomException;
 import com.amp.global.security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -44,5 +49,25 @@ public class UserFestivalService {
         );
 
         return UserFestivalPageResponse.of(festivalList);
+    }
+
+    @Transactional
+    public WishListResponse toggleWishlist(Long festivalId, WishListRequest request) {
+        User user = authService.getCurrentUser();
+        Festival festival = festivalRepository.findById(festivalId)
+                .orElseThrow(() -> new CustomException(FestivalErrorCode.FESTIVAL_NOT_FOUND));
+
+
+        UserFestival userFestival = userFestivalRepository.findByUserAndFestival(user, festival)
+                .orElseGet(() -> UserFestival.builder()
+                        .user(user)
+                        .festival(festival)
+                        .build());
+
+        userFestival.updateWishList(request.wishList());
+
+        userFestivalRepository.save(userFestival);
+
+        return new WishListResponse(festival.getId(), userFestival.getWishList());
     }
 }
