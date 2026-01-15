9b57bcc4a8dd64ec73d21a731bfb165be4f1851c
Choi Min Young <codingmy@naver.com>
2026-01-15 15:22:05 +0900

    [feat] 조회 성공 메세지 추가

--- src/main/java/com/amp/global/common/SuccessStatus.java
+++ src/main/java/com/amp/global/common/SuccessStatus.java
@@ -23,6 +23,7 @@ public enum SuccessStatus implements SuccessCode {
 
     // NOTICE
     NOTICE_DETAIL_GET_SUCCESS(HttpStatus.OK, "NOT", "001", "공지 상세 조회되었습니다."),
+    NOTICE_LIST_GET_SUCCESS(HttpStatus.OK,  "NOT", "002", "공지 목록 조회되었습니다."),
 
     // NOTICE SAVE
     BOOKMARK_UPDATE_SUCCESS(HttpStatus.CREATED, "BOK", "001", "북마크 요청이 정상적으로 처리되었습니다."),
