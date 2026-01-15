529cf77ecb75cddaec8c8ee83c54ae841bbade2c
dongchan0105 <come753zz@gmail.com>
2026-01-16 01:49:21 +0900

    [feat] 유저가 저장된 Response dto 개발 및 Repository에서 fetch join 으로 끌어옴

---
+++ src/main/java/com/amp/domain/user/dto/response/SavedAnnouncementResponse.java
@@ -0,0 +1,36 @@
+package com.amp.domain.user.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class SavedAnnouncementResponse {
+    private List<SavedAnnouncementDto> notices;
+    private PaginationDto pagination;
+
+    @Getter
+    @Builder
+    public static class SavedAnnouncementDto {
+        private Long savedNoticeId;
+        private Long noticeId;
+        private String festivalTitle;
+        private String categoryName;
+        private String title;
+        private String imageUrl;
+    }
+
+    @Getter
+    @Builder
+    public static class PaginationDto {
+        private int currentPage;
+        private int totalPages;
+        private long totalElements;
+        private int size;
+        private boolean hasNext;
+        private boolean hasPrevious;
+    }
+
+}
