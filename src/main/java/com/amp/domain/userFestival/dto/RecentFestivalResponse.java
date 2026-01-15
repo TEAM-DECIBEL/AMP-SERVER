4b905b0664bb5fd83beb377356cdc3110951a82b
Chae-Yu <chaeyu2004@naver.com>
2026-01-15 23:06:18 +0900

    [fix] 선언되지 않은 클래스들 추가

---
+++ src/main/java/com/amp/domain/userFestival/dto/RecentFestivalResponse.java
@@ -0,0 +1,40 @@
+package com.amp.domain.userFestival.dto;
+
+import com.amp.domain.festival.entity.Festival;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecentFestivalResponse {
+
+    private Long festivalId;
+    private String title;
+    private String mainImageUrl;
+    private String location;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Long dDay;
+
+    public static RecentFestivalResponse from(Festival festival) {
+        long dDay = ChronoUnit.DAYS.between(LocalDate.now(), festival.getStartDate());
+
+        return RecentFestivalResponse.builder()
+                .festivalId(festival.getId())
+                .title(festival.getTitle())
+                .mainImageUrl(festival.getMainImageUrl())
+                .location(festival.getLocation())
+                .startDate(festival.getStartDate())
+                .endDate(festival.getEndDate())
+                .dDay(dDay)
+                .build();
+    }
+
+}
