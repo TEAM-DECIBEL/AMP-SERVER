20edbeb02822bc4752ed5088f522054ea3f3dd66
Chae-Yu <chaeyu2004@naver.com>
2026-01-15 13:28:39 +0900

    [feat] 전체 공연 조회 API service 로직 작성

---
+++ src/main/java/com/amp/domain/userFestival/service/UserFestivalService.java
@@ -0,0 +1,48 @@
+package com.amp.domain.userFestival.service;
+
+import com.amp.domain.userFestival.dto.response.UserFestivalListResponse;
+import com.amp.domain.userFestival.dto.response.UserFestivalPageResponse;
+import com.amp.domain.festival.entity.Festival;
+import com.amp.domain.festival.repository.FestivalRepository;
+import com.amp.domain.user.entity.User;
+import com.amp.domain.userFestival.repository.UserFestivalRepository;
+import com.amp.global.security.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserFestivalService {
+
+    private final FestivalRepository festivalRepository;
+    private final UserFestivalRepository userFestivalRepository;
+    private final AuthService authService;
+
+    @Transactional(readOnly = true)
+    public UserFestivalPageResponse getAllFestivalLists(Pageable pageable) {
+        User user = authService.getCurrentUserOrNull();
+        Page<Festival> festivalPage = festivalRepository.findAllByDeletedAtIsNull(pageable);
+
+        if (festivalPage.isEmpty()) {
+            return UserFestivalPageResponse.of(Page.empty(pageable));
+        }
+
+        Set<Long> wishlistIds = (user != null)
+                ? userFestivalRepository.findAllFestivalIdsByUserId(user.getId())
+                : Collections.emptySet();
+
+        Page<UserFestivalListResponse> festivalList = festivalPage.map(f ->
+                UserFestivalListResponse.from(f, wishlistIds.contains(f.getId()))
+        );
+
+        return UserFestivalPageResponse.of(festivalList);
+    }
+}
