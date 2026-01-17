4ecf0237bf31feb302254f4827eff486c2abb89e
Chae-Yu <chaeyu2004@naver.com>
2026-01-17 21:05:49 +0900

    [feat] 혼잡도 repository 작성

---
+++ src/main/java/com/amp/domain/stage/repository/StageCongestionRepository.java
@@ -0,0 +1,7 @@
+package com.amp.domain.stage.repository;
+
+import com.amp.domain.stage.entity.StageCongestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StageCongestionRepository extends JpaRepository<StageCongestion, Long> {
+}
