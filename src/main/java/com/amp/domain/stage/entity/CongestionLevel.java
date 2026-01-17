8b9af962c558949a59f5fe49f5759b0acc7018e7
Chae-Yu <chaeyu2004@naver.com>
2026-01-17 21:05:40 +0900

    [feat] 혼잡도 레벨 enum 작성

--- src/main/java/com/amp/domain/stage/entity/CongestionLevel.java
+++ src/main/java/com/amp/domain/stage/entity/CongestionLevel.java
@@ -1,7 +1,22 @@
 package com.amp.domain.stage.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum CongestionLevel {
-    SMOOTH,          // 여유
-    NORMAL,          // 보통
-    CROWDED,         // 혼잡
+    SMOOTH("여유", 0, 30),
+    NORMAL("보통", 31, 70),
+    CROWDED("혼잡", 71, 100);
+
+    private final String description;
+    private final int minPercent;
+    private final int maxPercent;
+
+    public static CongestionLevel fromPercent(int percent) {
+        if (percent <= 30) return SMOOTH;
+        if (percent <= 70) return NORMAL;
+        return CROWDED;
+    }
 }
