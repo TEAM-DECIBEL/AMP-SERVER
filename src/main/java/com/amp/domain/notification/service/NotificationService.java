f4d0673c671b3dc0a074e61db9783f510a95e115
Choi Min Young <codingmy@naver.com>
2026-01-18 22:53:42 +0900

    [feat] 공지 생성 이벤트 기반 알림 전송 처리

---
+++ src/main/java/com/amp/domain/notification/service/NotificationService.java
@@ -0,0 +1,28 @@
+package com.amp.domain.notification.service;
+
+import com.amp.domain.notice.event.NoticeCreatedEvent;
+import com.amp.global.fcm.service.FCMService;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final FCMService fcmService;
+
+    public void sendNewNoticeNotification(NoticeCreatedEvent event) throws FirebaseMessagingException {
+
+        String title = event.getCategoryName()+" 공지가 업로드 되었어요!";
+        String body = "["+event.getCategoryName()+"]" +event.getTitle();
+
+
+        fcmService.sendCategoryTopicAlarm(
+                event.getCategoryId(),
+                title,
+                body
+        );
+    }
+}
+
