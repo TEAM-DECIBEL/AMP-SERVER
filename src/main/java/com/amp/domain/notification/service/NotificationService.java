b5856229add3d9659e1d2e3c7ff5108622a39692
Choi Min Young <codingmy@naver.com>
2026-01-19 00:11:52 +0900

    [refactor] 알림 내 시간 정보 표기에 공통모듈 적용

--- src/main/java/com/amp/domain/notification/service/NotificationService.java
+++ src/main/java/com/amp/domain/notification/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.amp.domain.notification.service;
 
 import com.amp.domain.notice.event.NoticeCreatedEvent;
+import com.amp.global.common.dto.TimeFormatter;
 import com.amp.global.fcm.service.FCMService;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +16,8 @@ public class NotificationService {
     public void sendNewNoticeNotification(NoticeCreatedEvent event) throws FirebaseMessagingException {
 
         String title = event.getCategoryName()+" 공지가 업로드 되었어요!";
-        String body = "["+event.getCategoryName()+"]" +event.getTitle();
-
+        String noticeBody = "["+event.getCategoryName()+"]" +event.getTitle();
+        String timeData = TimeFormatter.formatTimeAgo(event.getCreatedAt());
 
         fcmService.sendCategoryTopicAlarm(
                 event.getCategoryId(),
