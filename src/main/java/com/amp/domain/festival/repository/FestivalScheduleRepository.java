a49c220b8ed2e0d9463aef9f2471d11e96bb9013
Chae-Yu <chaeyu2004@naver.com>
2026-01-18 23:18:13 +0900

    [feat] 현장 혼잡도 입력 API service, repository 작성

--- src/main/java/com/amp/domain/festival/repository/FestivalScheduleRepository.java
+++ src/main/java/com/amp/domain/festival/repository/FestivalScheduleRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface FestivalScheduleRepository extends JpaRepository<FestivalSchedule, Long> {
     @Modifying(clearAutomatically = true)
     @Query("UPDATE FestivalSchedule f SET f.deletedAt = CURRENT_TIMESTAMP WHERE f.festival.id = :festivalId AND f.deletedAt IS NULL")
     void softDeleteByFestivalId(@Param("festivalId") Long festivalId);
 
+    @Query("SELECT fs FROM FestivalSchedule fs " +
+            "WHERE fs.festival.id = :festivalId " +
+            "ORDER BY fs.festivalDate")
+    List<FestivalSchedule> findByFestivalIdOrderByFestivalDate(@Param("festivalId") Long festivalId);
 }
