a9ac91f72ff3dabad7c6a4cfe6fe9b86ca39ed6f
Choi Min Young <codingmy@naver.com>
2026-01-19 00:22:53 +0900

    [refactor] FCM 알림 발행 실패시를 고려해 공지글 저장/FCM 알림 전송기능을 트랜잭션 분리

--- src/main/java/com/amp/domain/notification/listener/NotificationEventListener.java
+++ src/main/java/com/amp/domain/notification/listener/NotificationEventListener.java
@@ -2,19 +2,30 @@ package com.amp.domain.notification.listener;
 
 import com.amp.domain.notice.event.NoticeCreatedEvent;
 import com.amp.domain.notification.service.NotificationService;
-import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationEventListener {
 
     private final NotificationService notificationService;
 
-    @EventListener
-    public void handle(NoticeCreatedEvent event) throws FirebaseMessagingException {
-        notificationService.sendNewNoticeNotification(event);
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(NoticeCreatedEvent event) {
+        try {
+            notificationService.sendNewNoticeNotification(event);
+        } catch (Exception e) {
+            log.error(
+                    "[FCM] noticeId={} categoryId={} 알림 전송 실패",
+                    event.getNoticeId(),
+                    event.getCategoryId(),
+                    e
+            );
+        }
     }
 }
