4dd1053126d6805e4ccc2fc9ef6bb4385f977a71
Chae-Yu <chaeyu2004@naver.com>
2026-01-19 12:47:08 +0900

    [refactor] validated 어노테이션 추가

--- src/main/java/com/amp/domain/user/controller/UserController.java
+++ src/main/java/com/amp/domain/user/controller/UserController.java
@@ -17,6 +17,7 @@ import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 @Tag(name = "User API")
+@Validated
 @RequiredArgsConstructor
 public class UserController {
 
