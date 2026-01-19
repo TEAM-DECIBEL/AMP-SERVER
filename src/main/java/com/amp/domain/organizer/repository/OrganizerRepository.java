480351b46185ab974f4d1669d66ba8afa808dfc6
dongchan0105 <come753zz@gmail.com>
2026-01-19 22:25:36 +0900

    [feat] 주최사 MYPAGE의 response Dto 생성 및 주최사 정보 조회 와 공연 개수 조회 하는 repo로직 작성

--- src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
+++ src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
@@ -1,6 +1,7 @@
 package com.amp.domain.organizer.repository;
 
 import com.amp.domain.festival.entity.Festival;
+import com.amp.domain.festival.entity.FestivalStatus;
 import com.amp.domain.organizer.entity.Organizer;
 import com.amp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,10 +9,25 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+
+    Optional<Organizer> findByUserId(Long userId);
+
     boolean existsByFestivalAndUser(Festival festival, User user);
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Organizer o SET o.deletedAt = CURRENT_TIMESTAMP WHERE o.festival.id = :festivalId AND o.deletedAt IS NULL")
     void softDeleteByFestivalId(@Param("festivalId") Long festivalId);
+
+    @Query("SELECT COUNT(f) FROM Organizer o " +
+            "JOIN Festival f ON o.id = f.id " +
+            "WHERE o.id = :organizerId " +
+            "AND f.status = :status " +
+            "AND f.deletedAt IS NULL")
+    Integer countFestivalsByOrganizerIdAndStatus(
+            @Param("organizerId") Long organizerId,
+            @Param("status") FestivalStatus status
+    );
 }
