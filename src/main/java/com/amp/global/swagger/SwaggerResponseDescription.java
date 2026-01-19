615fed9b75b835efe57a8270dc06783a08da83e9
Choi Min Young <codingmy@naver.com>
2026-01-19 23:15:46 +0900

    [chore] 스웨거 문서에 추가

--- src/main/java/com/amp/global/swagger/SwaggerResponseDescription.java
+++ src/main/java/com/amp/global/swagger/SwaggerResponseDescription.java
@@ -7,6 +7,7 @@ import com.amp.domain.notice.exception.NoticeErrorCode;
 import com.amp.domain.user.exception.UserErrorCode;
 import com.amp.global.common.CommonErrorCode;
 import com.amp.global.common.ErrorCode;
+import com.amp.global.fcm.exception.FCMErrorCode;
 import com.amp.global.s3.S3ErrorCode;
 import lombok.Getter;
 
@@ -124,6 +125,20 @@ public enum SwaggerResponseDescription {
             NoticeErrorCode.DELETE_NOTICE_FAIL,
             UserErrorCode.USER_NOT_FOUND,
             NoticeErrorCode.NOTICE_DELETE_FORBIDDEN
+    ))),
+
+    // 카테고리 구독 API
+    FAIL_TO_SUBSCRIBE(new LinkedHashSet<>(Set.of(
+            UserErrorCode.USER_NOT_FOUND,
+            FestivalCategoryErrorCode.NOTICE_CATEGORY_NOT_FOUND,
+            FCMErrorCode.ALREADY_SUBSCRIBED
+    ))),
+
+    //카테고리 구독 취소 API
+    FAIL_TO_UNSUBSCRIBE(new LinkedHashSet<>(Set.of(
+            UserErrorCode.USER_NOT_FOUND,
+            FestivalCategoryErrorCode.NOTICE_CATEGORY_NOT_FOUND,
+            FCMErrorCode.NOT_SUBSCRIBED_CATEGORY
     )));
 
     private final Set<ErrorCode> errorCodeList;
