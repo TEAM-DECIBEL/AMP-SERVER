934a4c6b4f4b16e0e6eb7cd47dd24c8f1d21a959
Choi Min Young <codingmy@naver.com>
2026-01-19 22:35:37 +0900

    [feat] 성공 실패코드 추가

--- src/main/java/com/amp/global/fcm/exception/FCMErrorCode.java
+++ src/main/java/com/amp/global/fcm/exception/FCMErrorCode.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum FCMErrorCode implements ErrorCode {
 
+    // 400 Bad Request
+    ALREADY_SUBSCRIBED(HttpStatus.BAD_REQUEST, "FCM", "001", "이미 구독 중인 카테고리입니다."),
+    NOT_SUBSCRIBED_CATEGORY(HttpStatus.BAD_REQUEST, "FCM", "001", "구독하지 않은 카테고리입니다."),
+
     // 500 Internal Server Error
     FAIL_TO_SEND_PUSH_ALARM(HttpStatus.INTERNAL_SERVER_ERROR, "FCM", "001", "푸시알림 발송에 실패했습니다.");
 
