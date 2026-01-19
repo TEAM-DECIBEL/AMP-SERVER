2221202f7cbd2c0e8dc74fc0cf95642df4f4b0dd
Chae-Yu <chaeyu2004@naver.com>
2026-01-19 11:45:27 +0900

    [chore] swagger description 추가

--- src/main/java/com/amp/domain/stage/controller/StageCongestionController.java
+++ src/main/java/com/amp/domain/stage/controller/StageCongestionController.java
@@ -2,8 +2,10 @@ package com.amp.domain.stage.controller;
 
 import com.amp.domain.stage.dto.request.CongestionReportRequest;
 import com.amp.domain.stage.service.CongestionReportService;
+import com.amp.global.annotation.ApiErrorCodes;
 import com.amp.global.common.SuccessStatus;
 import com.amp.global.response.success.BaseResponse;
+import com.amp.global.swagger.SwaggerResponseDescription;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
@@ -22,6 +24,7 @@ public class StageCongestionController {
 
     @PostMapping
     @Operation(summary = "현장 혼잡도 입력")
+    @ApiErrorCodes(SwaggerResponseDescription.FAIL_TO_INPUT_CONGESTION)
     public ResponseEntity<BaseResponse<Void>> reportCongestion(
             @PathVariable @Positive Long stageId,
             @RequestBody @Valid CongestionReportRequest request) {
