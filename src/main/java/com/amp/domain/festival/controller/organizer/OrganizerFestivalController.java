82f3ac42eab35b7e90a86253061581fd2b7ddf1d
Chae-Yu <chaeyu2004@naver.com>
2026-01-19 12:35:44 +0900

    [refactor] validated 어노테이션 추가

--- src/main/java/com/amp/domain/festival/controller/organizer/OrganizerFestivalController.java
+++ src/main/java/com/amp/domain/festival/controller/organizer/OrganizerFestivalController.java
@@ -17,12 +17,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/organizer/me/festivals")
 @Tag(name = "Organizer API")
 @RequiredArgsConstructor
--- src/main/java/com/amp/domain/festival/controller/user/AudienceFestivalController.java
+++ src/main/java/com/amp/domain/festival/controller/user/AudienceFestivalController.java
@@ -13,12 +13,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/users/festivals")
 @Tag(name = "User API")
 @RequiredArgsConstructor
