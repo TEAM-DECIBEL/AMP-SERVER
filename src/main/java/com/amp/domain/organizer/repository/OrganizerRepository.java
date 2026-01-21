268de831427f5b967ec298be1db29bdd255e1b53
dongchan0105 <come753zz@gmail.com>
2026-01-22 05:31:42 +0900

    [fix] 채유를 고쳐라

--- src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
+++ src/main/java/com/amp/domain/organizer/repository/OrganizerRepository.java
@@ -1,8 +1,11 @@
 package com.amp.domain.organizer.repository;
 
+import com.amp.domain.festival.entity.Festival;
 import com.amp.domain.organizer.entity.Organizer;
 import com.amp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +14,11 @@ public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
     Boolean existsByUser(User user);
 
     Optional<Organizer> findByUserId(Long userId);
+
+    @Query("SELECT CASE WHEN COUNT(o) > 0 THEN true ELSE false END " +
+            "FROM Organizer o JOIN o.festivals f " +
+            "WHERE f = :festival AND o.user = :user")
+    Boolean existsByFestivalAndUser(@Param("festival") Festival festival,
+                                    @Param("user") User user);
 
 }
