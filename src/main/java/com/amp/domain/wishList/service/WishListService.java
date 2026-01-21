ca5c42cd3fd43b4a93467969fa86e339876a5c7c
Chae-Yu <chaeyu2004@naver.com>
2026-01-21 17:30:47 +0900

    [rename] UserFestival -> wishlist 로 이름 변경

--- src/main/java/com/amp/domain/wishList/service/WishListService.java
+++ src/main/java/com/amp/domain/wishList/service/WishListService.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserFestivalService {
+public class WishListService {
 
     private final WishListRepository userFestivalRepository;
     private final FestivalRepository festivalRepository;
