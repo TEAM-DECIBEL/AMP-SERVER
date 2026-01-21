9502efe93c3ed6b27f006058506c585baa9c9d78
Chae-Yu <chaeyu2004@naver.com>
2026-01-21 18:25:30 +0900

    [feat] 카테고리 필터링 로직 추가

--- src/main/java/com/amp/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/amp/domain/notice/repository/NoticeRepository.java
@@ -6,6 +6,8 @@ import com.amp.domain.notice.entity.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,4 +19,11 @@ public interface NoticeRepository extends JpaRepository<Notice, Long> {
     void deleteAllByFestivalAndFestivalCategory(Festival festival, FestivalCategory festivalCategory);
 
     Page<Notice> findAllByFestival(Festival festival, Pageable pageable);
+
+    @Query("SELECT n FROM Notice n " +
+            "WHERE n.festival = :festival " +
+            "AND (:categoryId IS NULL OR n.festivalCategory.category.id = :categoryId) " +
+            "AND n.deletedAt IS NULL " +
+            "ORDER BY n.isPinned DESC, n.createdAt DESC")
+    Page<Notice> findNoticesByFilter(@Param("festival") Festival festival, @Param("categoryId") Long categoryId, Pageable pageable);
 }
