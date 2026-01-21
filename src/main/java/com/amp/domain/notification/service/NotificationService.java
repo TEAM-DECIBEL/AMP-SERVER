8dd86181323217657ae1585dd06df0139d421192
Choi Min Young <codingmy@naver.com>
2026-01-22 01:29:02 +0900

    [feat] 유저의 notification 읽기 처리 추가

--- src/main/java/com/amp/domain/notification/service/NotificationService.java
+++ src/main/java/com/amp/domain/notification/service/NotificationService.java
@@ -1,17 +1,37 @@
 package com.amp.domain.notification.service;
 
 import com.amp.domain.notice.event.NoticeCreatedEvent;
+import com.amp.domain.notification.dto.response.NotificationListResponse;
+import com.amp.domain.notification.dto.response.NotificationResponse;
+import com.amp.domain.notification.entity.Alarm;
+import com.amp.domain.notification.entity.Notification;
+import com.amp.domain.notification.exception.NotificationErrorCode;
+import com.amp.domain.notification.exception.NotificationException;
+import com.amp.domain.notification.repository.AlarmRepository;
+import com.amp.domain.notification.repository.NotificationRepository;
+import com.amp.domain.user.entity.User;
+import com.amp.domain.user.exception.UserErrorCode;
+import com.amp.domain.user.repository.UserRepository;
 import com.amp.global.common.dto.TimeFormatter;
+import com.amp.global.exception.CustomException;
 import com.amp.global.fcm.service.FCMService;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
     private final FCMService fcmService;
+    private final AlarmRepository alarmRepository;
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
 
     public void sendNewNoticeNotification(NoticeCreatedEvent event) throws FirebaseMessagingException {
 
@@ -48,6 +68,26 @@ public class NotificationService {
                         TimeFormatter.formatTimeAgo(n.getCreatedAt())
                 ))
                 .toList());
+    }
+
+    @Transactional
+    public void readNotification(Long notificationId) {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(UserErrorCode.USER_NOT_FOUND));
+
+
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new NotificationException(NotificationErrorCode.NOTIFICATION_NOT_FOUND));
+
+        if (!notification.getUser().equals(user)) {
+            throw new NotificationException(NotificationErrorCode.NOTIFICATION_FORBIDDEN);
+        }
+
+        notification.markAsRead();
     }
 
 }
