81645647039da38fa58af943a7595283ab48594c
Chae-Yu <chaeyu2004@naver.com>
2026-02-28 15:05:31 +0900

    [refactor] 토큰 유효성 검증 로직 추가

--- src/main/java/com/amp/domain/notification/controller/FcmTokenController.java
+++ src/main/java/com/amp/domain/notification/controller/FcmTokenController.java
@@ -6,6 +6,7 @@ import com.amp.global.common.SuccessStatus;
 import com.amp.global.response.success.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class FcmTokenController {
     @Operation(summary = "FCM 토큰 기기 동기화", description = "로그인한 기기의 FCM 토큰을 현재 구독 중인 모든 카테고리 토픽에 등록합니다.")
     @PostMapping("/fcm-token")
     public ResponseEntity<BaseResponse<Void>> registerFcmToken(
-            @RequestBody FcmTopicSubscribeRequest fcmTopicSubscribeRequest
+            @RequestBody @Valid FcmTopicSubscribeRequest fcmTopicSubscribeRequest
     ) {
         categorySubscribeService.registerToken(fcmTopicSubscribeRequest.fcmToken());
         return ResponseEntity
