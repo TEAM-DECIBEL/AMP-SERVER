08ca46856f3dbf606f416522cf6e5e15d55ac732
Chae-Yu <chaeyu2004@naver.com>
2026-03-04 22:59:56 +0900

    [fix] 테스트 및 상수 오류 수정

--- src/test/java/com/amp/global/security/service/AuthServiceTest.java
+++ src/test/java/com/amp/global/security/service/AuthServiceTest.java
@@ -1,6 +1,5 @@
 package com.amp.global.security.service;
 
-import com.amp.domain.user.entity.Audience;
 import com.amp.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,14 +34,12 @@ class AuthServiceTest {
     @Test
     @DisplayName("로그인한 Audience 유저는 닉네임 반환")
     void getDisplayNicknameLoggedInAudience() {
-        Audience audience = Audience.builder().nickname("앰프").build();
-
         Authentication auth = mock(Authentication.class);
         given(auth.isAuthenticated()).willReturn(true);
         given(auth.getName()).willReturn("amp@amp.com");
         SecurityContextHolder.getContext().setAuthentication(auth);
 
-        given(userRepository.findByEmail("amp@amp.com")).willReturn(Optional.of(audience));
+        given(userRepository.findNicknameByEmail("amp@amp.com")).willReturn(Optional.of("앰프"));
 
         assertThat(authService.getDisplayNickname()).isEqualTo("앰프");
     }
