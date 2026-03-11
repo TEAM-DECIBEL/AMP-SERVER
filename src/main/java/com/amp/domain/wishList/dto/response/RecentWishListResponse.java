49850cf6bc897268bfc0661f425cdfe039f3ff87
Chae-Yu <chaeyu2004@naver.com>
2026-03-11 16:51:38 +0900

    [hotfix] dday가 dDay로 표시되지 않는 문제 수정
    - Lombok 클래스로 @Getter에서 Jackson이 dday로 직렬화시키는 문제 해결을 위해 record로 수정

--- src/main/java/com/amp/domain/wishList/dto/response/RecentWishListResponse.java
+++ src/main/java/com/amp/domain/wishList/dto/response/RecentWishListResponse.java
@@ -2,38 +2,27 @@ package com.amp.domain.wishList.dto.response;
 
 import com.amp.domain.festival.entity.Festival;
 import com.amp.domain.festival.util.FestivalUtils;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class RecentWishListResponse {
-
-    private Long festivalId;
-    private String title;
-    private String mainImageUrl;
-    private String location;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private Long dDay;
-
+public record RecentWishListResponse(
+        Long festivalId,
+        String title,
+        String mainImageUrl,
+        String location,
+        LocalDate startDate,
+        LocalDate endDate,
+        Long dDay
+) {
     public static RecentWishListResponse from(Festival festival) {
-        return RecentWishListResponse.builder()
-                .festivalId(festival.getId())
-                .title(festival.getTitle())
-                .mainImageUrl(festival.getMainImageUrl())
-                .location(festival.getLocation())
-                .startDate(festival.getStartDate())
-                .endDate(festival.getEndDate())
-                .dDay(FestivalUtils.calculateDDay(festival.getStartDate(), festival.getEndDate()))
-                .build();
+        return new RecentWishListResponse(
+                festival.getId(),
+                festival.getTitle(),
+                festival.getMainImageUrl(),
+                festival.getLocation(),
+                festival.getStartDate(),
+                festival.getEndDate(),
+                FestivalUtils.calculateDDay(festival.getStartDate(), festival.getEndDate())
+        );
     }
-
 }
